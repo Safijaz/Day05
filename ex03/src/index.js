28a70151fb62a729122666d430da363778fe73d0
Safijaz <79872266+Safijaz@users.noreply.github.com>
2021-03-13 05:45:07 +0100

    Add files via upload

---
+++ ex03/src/index.js
@@ -0,0 +1,10 @@
+function main(){
+    // Only change code below this line
+    var myString = "FirstLinenewlinetabbackslashSecondLinenewlineThirdLine";
+        myString = "FirstLine\n\tSecondLine\n\ThirdLine";
+    // Only change code above this line
+     return myString;
+      
+    }
+    console.log(main());
+    module.exports = main;
