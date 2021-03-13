dd2436ca5a1938f2f8aa44430560fdeaf650e982
Safijaz <79872266+Safijaz@users.noreply.github.com>
2021-03-13 05:50:53 +0100

    Add files via upload

---
+++ ex14/src/index.js
@@ -0,0 +1,18 @@
+function main(){
+    // Only change code below this code
+    var myNoun ="dog";
+    var myVerb = "ran";
+    var myAdjective ="big";
+    var myAdverb = "quickly";
+    var WordBlanks = "myNoun" + " " + "myVerb " + "  " + "myAdjective "+ " " + "myAdverb" + "  " + ".";
+    
+    
+    
+
+    
+    // Only change code above this code
+     return WordBlanks;
+      
+    }
+    console.log(main());
+    module.exports = main;
