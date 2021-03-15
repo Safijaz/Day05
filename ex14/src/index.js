f5da615c36219a1a7f8f01076bc23b758a5d26ce
Safijaz <79872266+Safijaz@users.noreply.github.com>
2021-03-15 03:19:04 +0100

    Add files via upload

--- ex14/src/index.js
+++ ex14/src/index.js
@@ -4,7 +4,7 @@ function main(){
     var myVerb = "ran";
     var myAdjective ="big";
     var myAdverb = "quickly";
-    var WordBlanks = "myNoun" + " " + "myVerb " + "  " + "myAdjective "+ " " + "myAdverb" + "  " + ".";
+    var WordBlanks = "myNoun " + " " + "myVerb" + " " + "myAdjective"  + " " + "myAdverb"  + " ";
     
     
     
